ce64798c4e7f18d848e27406ddeae4fa2011f961
skzaki <102476773+skzaki@users.noreply.github.com>
2024-01-18 16:44:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function loadBooks() {
+    // Implement AJAX or fetch to load books dynamically
+    // Update the #bookList element with the retrieved book data
+    // Example: document.getElementById('bookList').innerHTML = "Book 1, Book 2, ...";
+}
